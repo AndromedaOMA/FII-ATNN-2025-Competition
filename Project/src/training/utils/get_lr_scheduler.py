6e2deb9129e41d152844702734715aee111f1986
AndrewIGD <andreistoleru03@gmail.com>
2026-01-06 10:16:31 +0200

    CosineAnnealingWarmRestarts

--- Project/src/training/utils/get_lr_scheduler.py
+++ Project/src/training/utils/get_lr_scheduler.py
@@ -16,6 +16,12 @@ def get_lr_scheduler(configs, optimizer):
                                                           factor=configs["training"]["factor"],
                                                           patience=configs["training"]["patience"],
                                                           threshold=configs["training"]["threshold"])
+    elif name == 'cosineannealingwarmrestarts' or name == 'cosinewarmrestarts':
+        print('CosineAnnealingWarmRestarts Learning Rate Scheduler loaded!')
+        return torch.optim.lr_scheduler.CosineAnnealingWarmRestarts(optimizer,
+                                                                    T_0=configs["training"]["T_0"],
+                                                                    T_mult=configs["training"].get("T_mult", 1),
+                                                                    eta_min=configs["training"].get("eta_min", 0))
     else:
         print('The Learning Rate Scheduler name you have entered is not supported!')
         sys.exit()
